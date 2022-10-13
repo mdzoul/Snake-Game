af5eb3ab71bc9b736c96b726ef1ef31320fb4d41
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-10-14 03:05:44 +0800

    Added highscores and data.txt which stores the highscores

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
 	screen.update()
-	time.sleep(0.1)
+	time.sleep(0.05)
 	
 	snake.move()
 
@@ -35,13 +35,17 @@ while game_is_on:
 
 	# Detect collision with wall
 	if snake.head.xcor() > 280 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -280:
-		game_is_on = False
-		scoreboard.game_over()
+		# game_is_on = False
+		# scoreboard.game_over()
+		scoreboard.reset()
+		snake.reset()
 
 	# Detect collision with tail
 	for segment in snake.segments[1:]:
 		if snake.head.distance(segment) < 5:
-			game_is_on = False
-			scoreboard.game_over()
+			# game_is_on = False
+			# scoreboard.game_over()
+			scoreboard.reset()
+			snake.reset()
 
 screen.exitonclick()
